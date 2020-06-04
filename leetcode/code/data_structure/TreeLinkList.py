8a883aa8a2569113af4c9f97111bc8e514a7c767
bin liu <wjtxlliubin@outlook.com>
2020-06-04 23:31:00 +0800

    add

--- leetcode/code/data_structure/TreeLinkList.py
+++ leetcode/code/data_structure/TreeLinkList.py
@@ -1,16 +1,24 @@
 class Node():
-    def __init__(self, item):
+    def __init__(self, item,left=None,right=None):
         self.root = item
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
 class TreeLinkList():
-    def __init__(self,item):
-        self._head = Node(item)
+    def __init__(self):
+        self._head = None
 
     def is_empty(self):
         return self._head.root is None
 
+    def add(self,item,left=None,right=None):
+        if self.is_empty():
+            self._head = Node(item,left=left,right=right)
+        else:
+            raise Exception('root is not None of head')
+
+    def append(self,):
+        pass
 
 
 
